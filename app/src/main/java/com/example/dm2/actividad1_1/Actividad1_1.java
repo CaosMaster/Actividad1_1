90a16a34d396df7e9cd68c4305353978e54d29dd
CaosMaster <mrtaman121@gmail.com>
2017-11-24 11:35:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dm2/actividad1_1/Actividad1_1.java
@@ -0,0 +1,15 @@
+package com.example.dm2.actividad1_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad1_1);
+
+
+    }
+}
